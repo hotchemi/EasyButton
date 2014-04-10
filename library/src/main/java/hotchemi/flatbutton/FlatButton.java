2fdce7b4e1f0b93116620f49c343f4efac4115fd
Shintaro Katafuchi <katafuchi_s@r.recruit.co.jp>
2014-04-10 13:32:44 +0900

    add sutom attributes.

--- library/src/main/java/hotchemi/flatbutton/FlatButton.java
+++ library/src/main/java/hotchemi/flatbutton/FlatButton.java
@@ -2,23 +2,27 @@
 package hotchemi.flatbutton;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
 import android.widget.Button;
 
 /**
- * Provides flat button.
+ * Provides flat style button.
  * 
  * @author Shintaro Katafuchi
  */
 public class FlatButton extends Button {
 
-
     public FlatButton(Context context) {
         super(context);
     }
 
-
-
-
-
+    public FlatButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.flat_button);
+        setText(a.getText(0));
+        setTextColor(a.getColor(1, 0));
+        setTextSize(a.getDimension(2, 0));
+    }
 
 }
